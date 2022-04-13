a148663b602b6cd4f01e774e3accc81b46bf3b47
Doug Stein <dougstein@gmail.com>
2022-04-12 22:15:38 -0700

    Initial Commit

---
+++ SweepData/SweepData.h
@@ -0,0 +1,18 @@
+//
+//  SweepData.h
+//  SweepData
+//
+//  Created by Doug on 4/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SweepData.
+FOUNDATION_EXPORT double SweepDataVersionNumber;
+
+//! Project version string for SweepData.
+FOUNDATION_EXPORT const unsigned char SweepDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SweepData/PublicHeader.h>
+
+
