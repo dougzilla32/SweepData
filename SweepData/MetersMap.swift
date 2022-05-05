ea83a3479de013ab6457716baff7ca75d8dd7f6f
Doug Stein <dougstein@gmail.com>
2022-05-05 03:07:28 -0700

    Add MetersPath
    LLPath common protocol for ParkingRegulationsPath and MetersPath

---
+++ SweepData/MetersMap.swift
@@ -0,0 +1,25 @@
+//
+//  MetersMap.swift
+//  SweepData
+//
+//  Created by Doug on 5/4/22.
+//
+
+public struct MeterPath: LLPath, BinaryCodable {
+    public let row: Int
+    public let postID: String
+    public let parkingSpaceID: Int
+    public let smartMeterFlag: String
+    public let capColor: MeterCapColor
+    public let streetID: Int?
+    public let streetName: String
+    public let streetNum: Int?
+    public let days: [Day]?
+    public let hoursBegin: HourAndMinute?
+    public let hoursEnd: HourAndMinute?
+    public let scheduleType: String
+    public let hourlyRate: Decimal?
+    public let timeLimit: HourAndMinute?
+    public let orientation: Int?
+    public let path: [LatitudeLongitude]
+}
--- SweepData/ParkingRegulationsMap.swift
+++ SweepData/ParkingRegulationsMap.swift
@@ -7,40 +7,41 @@
 
 public struct ParkingRegulationsMap: BinaryCodable {
     public let date: Date
-    public let segments: [ParkingRegulationsSegment]
+    public let segments: [ParkingRegulationPath]
     public let boundingBox: BoundingBox
     
-    public init(date: Date, segments: [ParkingRegulationsSegment], boundingBox: BoundingBox) {
+    public init(date: Date, segments: [ParkingRegulationPath], boundingBox: BoundingBox) {
         self.date = date
         self.segments = segments
         self.boundingBox = boundingBox
     }
 }
 
-public struct ParkingRegulationsSegment: BinaryCodable {
-    let row: Int
-    let regulation: String
-    let days: [Day]?
-    let hours: String
-    let hoursBegin: HourAndMinute?
-    let hoursEnd: HourAndMinute?
-    let regulationDetails: String
-    let timeLimit: HourAndMinute?
-    let exceptions: String
-    let fromTime: String
-    let toTime: String
-    let shapePath: [LatitudeLongitude]
+public struct ParkingRegulationPath: LLPath, BinaryCodable {
+    public let row: Int
+    public let regulation: String
+    public let days: [Day]?
+    public let hours: String
+    public let hoursBegin: HourAndMinute?
+    public let hoursEnd: HourAndMinute?
+    public let regulationDetails: String
+    public let timeLimit: HourAndMinute?
+    public let exceptions: String
+    public let fromTime: String
+    public let toTime: String
+    public let shapePath: [LatitudeLongitude]
 
-    let cnnRightLeft: RightLeft?
-    let left_fromAddress: Int?
-    let left_toAddress: Int?
-    let right_fromAddress: Int?
-    let right_toAddress: Int?
-    let streetName: String
-    let zipCode: Int?
-    let path: [LatitudeLongitude]
+    public let cnnRightLeft: RightLeft?
+    public let left_fromAddress: Int?
+    public let left_toAddress: Int?
+    public let right_fromAddress: Int?
+    public let right_toAddress: Int?
+    public let streetName: String
+    public let streetNum: Int?
+    public let zipCode: Int?
+    public let path: [LatitudeLongitude]
     
-    public init(row: Int, regulation: String, days: [Day]?, hours: String, hoursBegin: HourAndMinute?, hoursEnd: HourAndMinute?, regulationDetails: String, timeLimit: HourAndMinute?, exceptions: String, fromTime: String, toTime: String, shapePath: [LatitudeLongitude], cnnRightLeft: RightLeft?, left_fromAddress: Int?, left_toAddress: Int?, right_fromAddress: Int?, right_toAddress: Int?, streetName: String, zipCode: Int?, path: [LatitudeLongitude]) {
+    public init(row: Int, regulation: String, days: [Day]?, hours: String, hoursBegin: HourAndMinute?, hoursEnd: HourAndMinute?, regulationDetails: String, timeLimit: HourAndMinute?, exceptions: String, fromTime: String, toTime: String, shapePath: [LatitudeLongitude], cnnRightLeft: RightLeft?, left_fromAddress: Int?, left_toAddress: Int?, right_fromAddress: Int?, right_toAddress: Int?, streetName: String, streetNum: Int?, zipCode: Int?, path: [LatitudeLongitude]) {
         self.row = row
         self.regulation = regulation
         self.days = days
@@ -59,6 +60,7 @@ public struct ParkingRegulationsSegment: BinaryCodable {
         self.right_fromAddress = right_fromAddress
         self.right_toAddress = right_toAddress
         self.streetName = streetName
+        self.streetNum = streetNum
         self.zipCode = zipCode
         self.path = path
     }
