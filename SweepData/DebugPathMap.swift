ae332f9493b22949723f30693dc4a8548f023573
dougzilla32 <dougstein@gmail.com>
2022-06-18 22:27:19 +0900

    Simply LLPath, ParkingRegulationsPath, and MeterMapPatth to DebugPath

---
+++ SweepData/DebugPathMap.swift
@@ -0,0 +1,30 @@
+//
+//  DebugPathMap.swift
+//  SweepData
+//
+//  Created by Doug on 6/18/22.
+//
+
+public struct DebugPathMap: BinaryCodable {
+    public let date: Date
+    public let paths: [DebugPath]
+    public let boundingBox: BoundingBox
+    
+    public init(date: Date, paths: [DebugPath], boundingBox: BoundingBox) {
+        self.date = date
+        self.paths = paths
+        self.boundingBox = boundingBox
+    }
+}
+
+public struct DebugPath: BinaryCodable, Hashable {
+    public let row: Int
+    public let path: [LatitudeLongitude]
+    public let description: String
+    
+    public init(row: Int, path: [LatitudeLongitude], description: String) {
+        self.row = row
+        self.path = path
+        self.description = description
+    }
+}
