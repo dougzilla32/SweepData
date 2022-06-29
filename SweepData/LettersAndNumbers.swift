d162f80cb7283da70c3a5395e225a3883eab9fce
dougzilla32 <dougstein@gmail.com>
2022-06-29 11:10:48 +0900

    Move constants for LettersAndNumbers into SweepData (needed for IndicatorPoints calculation in SweepMap)

---
+++ SweepData/LettersAndNumbers.swift
@@ -0,0 +1,20 @@
+//
+//  LettersAndNumbers.swift
+//  SweepData
+//
+//  Created by Doug on 6/29/22.
+//
+
+public class LettersAndNumbers {
+    public static let DefaultLineWidth = 1.025 / 25000.0
+    public static let Size = DefaultLineWidth * 3.5
+    public static let FontSize = 36.0
+    public static let ScaledFontSize = 36.0 * 10
+    public static let TransformMultiplier = Size / FontSize
+    public static let ScaleMultiplier = Size / ScaledFontSize
+    public static let BoxWidth = Size
+    public static let BoxHeight = Size
+    
+    public static let Opaque: Float = 0.8
+    public static let Standard: Float = 0.55
+}
