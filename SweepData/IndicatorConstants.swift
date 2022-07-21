c0d718b523da2f9cb09fdd555ea3ed47fc49f1d0
dougzilla32 <dougstein@gmail.com>
2022-07-22 02:37:56 +0900

    Remove obsolete LettersAndNumbers and associated IndicatorConstants

--- SweepData/IndicatorConstants.swift
+++ SweepData/IndicatorConstants.swift
@@ -7,17 +7,5 @@
 
 public struct IndicatorConstants {
     public static let DefaultLineWidth = 1.025 / 25000.0
-    public static let FontSizeFactor = DefaultLineWidth * 3.5
-
-    public static let FontSize = 36.0
-    public static let FontScaledSize = 36.0 * 10
-    public static let FontTransformMultiplier = FontSizeFactor / FontSize
-    public static let FontScaleMultiplier = FontSizeFactor / FontScaledSize
-    public static let FontBoxWidth = FontSizeFactor
-    public static let FontBoxHeight = FontSizeFactor
-    
     public static let ImageSizeFactor = DefaultLineWidth * 4
-
-    public static let Opaque: Float = 0.8
-    public static let Standard: Float = 0.55
 }
