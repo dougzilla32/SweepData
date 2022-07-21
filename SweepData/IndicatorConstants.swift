4d6ce3d0416b96b204473b5ba24778d0e8083a97
dougzilla32 <dougstein@gmail.com>
2022-07-21 11:13:29 +0900

    Remove unnecessary image constants from IndicatorConstants

--- SweepData/IndicatorConstants.swift
+++ SweepData/IndicatorConstants.swift
@@ -16,14 +16,7 @@ public struct IndicatorConstants {
     public static let FontBoxWidth = FontSizeFactor
     public static let FontBoxHeight = FontSizeFactor
     
-    public static let ImageSizeFactorWidth = DefaultLineWidth * 2.5 * 1.2
-    public static let ImageSizeFactorHeight = DefaultLineWidth * 2.5
-    public static let ImageScaleFactor = 10.0
-//    public static let ImageSize = 36.0
-//    public static let ImageScaledSize = ImageSize * 10
-//    public static let ImageScaleMultiplier = Size / ImageScaledSize
-//    public static let ImageBoxWidth = DefaultLineWidth * 2.5
-//    public static let ImageBoxHeight = DefaultLineWidth * 2.5
+    public static let ImageSizeFactor = DefaultLineWidth * 4
 
     public static let Opaque: Float = 0.8
     public static let Standard: Float = 0.55
