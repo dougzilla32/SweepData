f463418d84d2d78ca1aa8b0fe39f6c42b3ae724f
dougzilla32 <dougstein@gmail.com>
2022-07-22 11:45:21 +0900

    Nicely scale the line width when zoom in a lot
    Change RouteLoader.DefaultLineWidth references to IndicatorConstants.DefaultLineWidth

--- SweepData/IndicatorConstants.swift
+++ SweepData/IndicatorConstants.swift
@@ -8,4 +8,5 @@
 public struct IndicatorConstants {
     public static let DefaultLineWidth = 1.025 / 25000.0
     public static let ImageSizeFactor = DefaultLineWidth * 4
+    public static let ImageAnchorZoom = 17.75
 }
